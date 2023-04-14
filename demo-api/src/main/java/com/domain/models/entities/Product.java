0808e2e15cbc700eec5045f0b6dd357b9c2bab81
Aditya Prabowo <82871029+ditype@users.noreply.github.com>
2023-04-14 23:56:19 +0700

    mydev

---
+++ demo-api/src/main/java/com/domain/models/entities/Product.java
@@ -0,0 +1,71 @@
+package com.domain.models.entities;
+
+import java.io.Serializable;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class Product implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", length = 100)
+    private String username;
+
+    @Column(name = "email", length = 200)
+    private String email;
+
+    private String password;
+
+    public Product() {
+
+    }
+
+    public Product(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
